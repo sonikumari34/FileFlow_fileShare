a5b6ef66c5cc3d737894c847489d1109b7edabc2
soni kumari <sonikumari34624@gmail.com>
2025-05-04 19:22:17 +0530

    updated

--- client/src/config/axiosInstance.js
+++ client/src/config/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 const BASE_URL="/api/files/"
+
+//const BASE_URL = "https://fileflow-fileshare.onrender.com/api/files/";
 const axiosInstance=axios.create()
 
 axiosInstance.defaults.baseURL=BASE_URL;
